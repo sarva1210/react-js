0d4fb3cb667bf20c5964059e2fc2708e35aa2558
sarva1210 <sarva.vish77@gmail.com>
2025-12-18 21:58:04 +0530

    Day2

---
+++ Day2/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1>Hello guys, I am Sarva</h1>
+      <h2>Cohort 2.0 is a very good batch 🚀</h2>
+    </div>
+  )
+}
+
+export default App
