b5974cd4e2d0db25ced1d970060e6ac7e142b420
sarva1210 <sarva.vish77@gmail.com>
2025-12-19 22:00:00 +0530

    Day3

---
+++ Day3/src/App.jsx
@@ -0,0 +1,13 @@
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+
+const App = () => {
+  return (
+    <>
+      <Nav />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
