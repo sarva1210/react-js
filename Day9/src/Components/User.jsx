b601d129b9b36448873f2315cf726b8f01a4d029
sarva1210 <sarva.vish77@gmail.com>
2025-12-30 21:52:26 +0530

    day9 (working)

---
+++ Day9/src/Components/User.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const User = () => {
+  const clr1 = Math.floor(Math.random() * 256);
+  const clr2 = Math.floor(Math.random() * 256);
+  const clr3 = Math.floor(Math.random() * 256);
+
+  return (
+    <div
+      className="user-card"
+      style={{ backgroundColor: `rgb(${clr1}, ${clr2}, ${clr3})` }}>
+    </div>
+  );
+};
+
+export default User;
+
