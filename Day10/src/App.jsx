a4f543fae0ed42fb5de26faaa9003f8b840092e2
sarva1210 <sarva.vish77@gmail.com>
2025-12-31 22:13:56 +0530

    Day10(completed)

--- Day10/src/App.jsx
+++ Day10/src/App.jsx
@@ -1,9 +1,40 @@
-import React from "react"
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import './App.css'
 
-const App=()=>{
-  return(
-    <h1>App</h1>
+const App = () => {
+
+  const [username, setUsername] = useState('')
+  const [num, setNum] = useState(0)
+  const [image, setImage] = useState('')
+
+  const getData = async () => {
+    const response = await axios.get('https://randomuser.me/api/')
+    const user = response.data.results[0]
+
+    setUsername(`${user.name.first} ${user.name.last}`)
+    setImage(user.picture.large)
+  }
+
+  useEffect(() => {
+    getData()
+  }, [num])
+
+  return (
+    <div className="app">
+
+      <img src={image} alt="User" className="avatar" />
+
+      <div className="username">{username}</div>
+
+      <div className="counter">{num}</div>
+
+      <button onClick={() => setNum(num + 1)}>
+        Fetch New User
+      </button>
+    </div>
   )
 }
 
 export default App
+
