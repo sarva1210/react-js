518974652fce68119bf6b96314fd188da338d3d2
sarva1210 <sarva.vish77@gmail.com>
2025-12-31 20:22:04 +0530

    day9(completed)

--- Day9/src/App.jsx
+++ Day9/src/App.jsx
@@ -1,33 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import User from "./Components/User";
 
 const App = () => {
 
-  const[allData, setAllData] = useState([])
+  const [allData, setAllData] = useState([]);
 
   const getData = async () => {
     const response = await axios.get("https://jsonplaceholder.typicode.com/users");
-    console.log(response.data);
-
-    setAllData(response.data)
+    setAllData(response.data);
   };
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   return (
     <div>
-      <button onClick={getData}>Get Data</button>
-
-      <div className='all-cards'>
-        {allData.map(function(elem,idx){
-          console.log(elem);
-          return<User/>
-        })}
-
+      <div className="all-cards">
+        {allData.map((elem, idx) => (
+          <User key={idx} elem={elem} />
+        ))}
       </div>
     </div>
   );
 };
 
 export default App;
+
 
 
--- Day9/src/Components/User.jsx
+++ Day9/src/Components/User.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const User = () => {
+const User = (props) => {
   const clr1 = Math.floor(Math.random() * 256);
   const clr2 = Math.floor(Math.random() * 256);
   const clr3 = Math.floor(Math.random() * 256);
@@ -9,6 +9,8 @@ const User = () => {
     <div
       className="user-card"
       style={{ backgroundColor: `rgb(${clr1}, ${clr2}, ${clr3})` }}>
+        <h2>{props.elem.name}</h2>
+        <p>{props.elem.email}</p>
     </div>
   );
 };
