79bb54372159a99ae7c7abc76a988a31f214f50b
sarva1210 <sarva.vish77@gmail.com>
2026-01-03 21:57:55 +0530

    day12 (started)

--- Day12/src/App.jsx
+++ Day12/src/App.jsx
@@ -1,34 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react"
+import Navbar from "./Components/Navbar"
+import About from "./Pages/About"
+import Home from "./Pages/Home"
+import Courses from "./Pages/Courses"
+import Kodr from "./Pages/Kodr"
+import Kodex from "./Pages/Kodex"
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/courses" element={<Courses />}>
+          <Route path="/courses/kodr" element={<Kodr />} />
+          <Route path="/courses/kodex" element={<Kodex />} />
+        </Route>
+      </Routes>
+    </div>
   )
 }
 
---
+++ Day12/src/Components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div className='flex items-center justify-between px-10 py-4 bg-gradient-to-r from-stone-700 to-stone-600 shadow-lg sticky top-0 z-50'>
+      <h1 className='text-stone-100 text-xl font-semibold tracking-wide' >Navbar</h1>
+      <input className='px-4 py-1 rounded-md bg-stone-100 text-stone-800 outline-none focus:ring-2 focus:ring-stone-400' type="text" />
+      <div className='flex gap-8 text-stone-100 font-medium'>
+        <Link className='hover:text-stone-300 transition' to='/'>Home</Link>
+        <Link className='hover:text-stone-300 transition' to='/About'>About</Link>
+        <Link className='hover:text-stone-300 transition' to='/Courses'>Courses</Link>
+        <Link className='hover:text-stone-300 transition' to='/Courses/Kodr'>Koder</Link>
+        <Link className='hover:text-stone-300 transition' to='/Courses/Kodex'>Kodex</Link>
+        <Link className='hover:text-stone-300 transition' to='/Courses'>Courses</Link>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
