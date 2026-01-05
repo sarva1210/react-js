928794c3f1f11d847592c45d470f3cf12b6ff618
sarva1210 <sarva.vish77@gmail.com>
2026-01-05 13:33:39 +0530

    project8(completed)

--- project8/src/Components/Sidebar.jsx
+++ project8/src/Components/Sidebar.jsx
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom";
 const Sidebar = () => {
   return (
     <div className="w-20 bg-zinc-900 flex flex-col items-center py-6 gap-10">
-      <span className="text-green-500 text-xl">♪</span>
+      <span className="text-green-400 text-xl">𝄞</span>
 
       <NavLink to="/" className="text-xl opacity-70 hover:opacity-100">
         🏠︎
