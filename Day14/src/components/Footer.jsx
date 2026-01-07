c4e17c1d56b72f017d717e6a63317a68cc68ed4a
sarva1210 <sarva.vish77@gmail.com>
2026-01-07 11:52:46 +0530

    Day14

---
+++ Day14/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className='absolute bottom-0 w-screen h-16 bg-blue-800'>
+        <h1>This is Footer</h1>
+        
+    </div>
+  )
+}
+
+export default Footer
---
+++ Day14/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Navbar = () => {
+
+  return (
+    <div className='h-16 w-full bg-emerald-900'>
+        <h1>This is Navbar </h1>
+
+    </div>
+  )
+}
+
+export default Navbar
