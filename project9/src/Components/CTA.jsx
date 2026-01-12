05f5d6376b12ed5c8fdceb56d51a9e67435ad44c
sarva1210 <sarva.vish77@gmail.com>
2026-01-12 18:44:27 +0530

    project9(CTA section)

--- project9/src/Components/CTA.jsx
+++ project9/src/Components/CTA.jsx
@@ -1,2 +1,18 @@
 import React from "react";
 
+export default function CTA() {
+  return (
+    <section className="max-w-[1280px] mx-auto px-6 md:px-16 my-32">
+
+        <div className="bg-white rounded-[32px] p-16 text-center shadow-sm">
+
+            <p className="text-sm text-purpleMain mb-4">Open the door to forging your brand’s story</p>
+
+            <h2 className="text-[34px] md:text-[40px] font-medium leading-[1.2]"> Embark on a Transformative Journey <br /> Shaping{" "}
+                <span className="bg-yellowMain px-3 rounded-md"> Your Brand’s Destiny </span>
+            </h2>
+        </div>
+
+    </section>
+  )
+}
