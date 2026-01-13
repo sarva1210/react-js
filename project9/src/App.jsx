9670ccebdc7e2be29ce351faad8e9e2e57a34b70
sarva1210 <sarva.vish77@gmail.com>
2026-01-13 18:03:12 +0530

    project9(done)

--- project9/src/App.jsx
+++ project9/src/App.jsx
@@ -2,6 +2,12 @@ import React from "react";
 import {Routes,Route} from "react-router-dom"
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
+import Home from "./Pages/Home";
+import ServicesPage from "./Pages/ServicesPage";
+import WorkPage from "./Pages/WorkPage";
+import AboutPage from "./Pages/AboutPage";
+import GalleryPage from "./Pages/GalleryPage";
+import ContactPage from "./Pages/ContactPage";
 
 export default function App(){
   return(
@@ -9,7 +15,12 @@ export default function App(){
      <Navbar/>
 
      <Routes>
-          <Route path ="/" element={<Home/>}/>
+        <Route path ="/" element={<Home/>}/>
+        <Route path ="/services" element={<ServicesPage/>}/>
+        <Route path ="/work" element={<WorkPage/>}/>
+        <Route path ="/about" element={<AboutPage/>}/>
+        <Route path ="/gallery" element={<GalleryPage/>}/>
+        <Route path ="/contact" element={<ContactPage/>}/>
      </Routes>
 
      <Footer/>
