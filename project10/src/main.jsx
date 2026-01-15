5df8091f5f4739b728bbd4aef8d77ee804995507
sarva1210 <sarva.vish77@gmail.com>
2026-01-15 12:55:58 +0530

    project10

--- project10/src/main.jsx
+++ project10/src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { AppProvider } from "./context/AppContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-    <App/>
+      <AppProvider>
+        <App />
+      </AppProvider>
     </BrowserRouter>
   </React.StrictMode>
 )
